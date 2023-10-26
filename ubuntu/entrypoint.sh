b6bb39584f9f0bc166cd25333b54dc54371d7c71
Sandro Mani <manisandro@gmail.com>
2023-10-26 10:22:39 +0200

    Use mkdir -p

--- ubuntu/entrypoint.sh
+++ ubuntu/entrypoint.sh
@@ -4,7 +4,7 @@
 if [ $LOCALE != 'en_US' ]; then
   echo "Compiling locale definition for $LOCALE"
   # NOTE: Write to /tmp/locale-archive to allow unprivileged users to create locales
-  mkdir /tmp/locale-archive
+  mkdir -p /tmp/locale-archive
   localedef -i $LOCALE -c -f UTF-8 -A /usr/share/locale/locale.alias /tmp/locale-archive/$LOCALE.UTF-8
   export LOCPATH=/tmp/locale-archive
   export LANG=${LOCALE}.UTF-8
